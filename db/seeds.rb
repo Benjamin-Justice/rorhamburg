f2348b896192eb91c6a259b5a8174aea799d3999
Benjamin Justice <benjamin.justice@namics.com>
2017-01-01 15:42:37 +0000

    db table quotes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Quote.create(saying: "blabla" , author: "Gigi D'Agostino")
